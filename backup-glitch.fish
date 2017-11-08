3d415af41668af1c92291a3e968797111ab2a0a3
Michiel Sikma <michiel@sikma.org>
2017-11-08 20:54:59 +0100

    Update target directory, add backup timestamp

--- backup-glitch.fish
+++ backup-glitch.fish
@@ -1,11 +1,13 @@
 #!/usr/bin/env fish
-if not test -d "/Volumes/Glitch/Music"
+if not test -d "/Volumes/Glitch/"
   echo "Can't access source directory: /Volumes/Glitch/"
   exit
 end
-if not test -d "/Volumes/Files/Music/Archive"
-  echo "Can't access target directory: /Volumes/Files/Music/Archive/"
+if not test -d "/Volumes/Files/Music/Glitch backup"
+  echo "Can't access target directory: /Volumes/Files/Music/Glitch backup/"
   exit
 end
 
-rsync -ahvrESH8 --delete --progress --stats --exclude=".*/" --exclude=".*" "/Volumes/Glitch/" "/Volumes/Files/Music/Archive/"
+rsync -ahvrESH8 --delete --progress --stats --exclude=".*/" --exclude=".*" "/Volumes/Glitch/" "/Volumes/Files/Music/Glitch backup/"
+rm -f "/Volumes/Files/Music/Glitch backup/.backup"
+date -u > "/Volumes/Files/Music/Glitch backup/.backup"
