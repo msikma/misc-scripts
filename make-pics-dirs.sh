55f389a63afaf8e6fb3d508eefd9bbd519c89647
Michiel Sikma <michiel@sikma.org>
2017-11-29 23:49:40 +0100

    Pictures dir scripts

---
+++ make-pics-dirs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+ln -s ~/Pictures/Collections/Animals ~/Pictures/Animals
+ln -s ~/Pictures/Collections/Avatars ~/Pictures/Avatars
+ln -s ~/Pictures/Collections/Foggy ~/Pictures/Foggy
+ln -s ~/Pictures/Collections/Games ~/Pictures/Games
+ln -s ~/Pictures/Collections/Tech ~/Pictures/Tech
+ln -s ~/Pictures/Collections/Icons ~/Pictures/Icons
+ln -s ~/Pictures/Collections/Informational ~/Pictures/Informational
+ln -s ~/Pictures/Collections/Mooie\ dingen ~/Pictures/Mooie\ dingen
+ln -s ~/Pictures/Collections/Pokémon ~/Pictures/Pokémon
+ln -s ~/Pictures/Collections/Software ~/Pictures/Software
+ln -s ~/Pictures/Collections/Stuff ~/Pictures/Stuff
+ln -s ~/Pictures/Collections/Design\ inspiration ~/Pictures/Design\ inspiration
+ln -s ~/Pictures/Collections/Winter ~/Pictures/Winter
+ln -s ~/Pictures/Collections/漫画 ~/Pictures/漫画
+ln -s ~/Pictures/Collections/電車 ~/Pictures/電車
+ln -s ~/Pictures/Collections/食べ物 ~/Pictures/食べ物
