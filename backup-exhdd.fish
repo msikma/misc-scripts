c6f4ae0f0a31e88b57eefc7326f092615d915227
Michiel Sikma <michiel@sikma.org>
2017-12-24 14:23:56 +0100

    Remove old exhdd script

--- backup-exhdd.fish
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env fish
-echo Backing up Prototype Stalker-C...
-rsync -avrSH8 --progress --delete --stats --exclude=".*/" "/Volumes/Prototype Stalker-C/." "/Volumes/Gencore Phoenix/Prototype Stalker-C Backup"
